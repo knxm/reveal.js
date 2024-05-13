b66ee9bf371e8a073e61b828964400a057c4a29a
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-05-14 08:54:39 +0900

    Fix font size

--- p5js/memory.js
+++ p5js/memory.js
@@ -7,14 +7,14 @@ const data1="00000001"
 const data2="00000011"
 let darray1=data1.split('')
 let darray2=data2.split('')
-function preload() {
+/* function preload() {
   font = loadFont("media/static/SourceCodePro-Regular.ttf");
-}
+}*/
 function setup() {
     createCanvas(400, 200);
-    textSize(16);
+    textSize(18);
     background(204);
-    textFont(font);
+/ *   textFont(font); */
 }
 
 function draw() {
@@ -34,7 +34,7 @@ function draw() {
     }
     if (c%n==2) {
         fill('black')
-	text("x = 1;   // 0d1 = 0b00000001", start*20, s*(c%n));
+	text("x = 1;    // 0d1 = 0b00000001", start*20, s*(c%n));
 	for (let j=0; j<8; j++) {
             text(darray1[j], 6+(start+j)*20, h+16);
 	}
@@ -55,7 +55,7 @@ function draw() {
     }
     if (c%n==5) {
         fill('black')
-	text("→3       // 0d3 = 0b00000011", start*20, s*(c%n));
+	text("→3        // 0d3 = 0b00000011", start*20, s*(c%n));
 	for (let j=0; j<8; j++) {
 	    text(darray2[j], 6+(start+j)*20, h+16);
 	}
