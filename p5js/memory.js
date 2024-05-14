871ecfc5084a4754f39f7cbbd64220784317eb00
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-05-14 14:53:40 +0900

    Fix memory.js with yellow box

--- p5js/memory.js
+++ p5js/memory.js
@@ -3,21 +3,23 @@ let start=4;
 let h=140;
 let s=20;
 let n=6;
-let font;
+// let font;
 const data1="00000001"
 const data2="00000011"
 let darray1=data1.split('')
 let darray2=data2.split('')
 
+/*
 function preload() {
   font = loadFont("media/static/SourceCodePro-Regular.ttf");
 }
+*/
 
 function setup() {
     createCanvas(400, 200);
     textSize(18);
     background(204);
-    textFont(font);
+   //  textFont(font);
 }
 
 function draw() {
@@ -35,16 +37,22 @@ function draw() {
 	    rect(i*20, h, 20, 20);
 	}
     }
+    if ((c%n) >0 && (c%n) < n) {
+    for (let i=start; i<start+8; i++){
+	    fill('yellow');
+	    rect(i*20, h, 20, 20);
+	}
+    }
     if (c%n==2) {
         fill('black')
-	text("x = 1;    // 0d1 = 0b00000001", start*20, s*(c%n));
+	text("x = 1;     // 0d1 = 0b00000001", start*20, s*(c%n));
 	for (let j=0; j<8; j++) {
             text(darray1[j], 6+(start+j)*20, h+16);
 	}
     }
     if (c%n==3) {
         fill('black')
-	text("x += 2;    // 0d2 = 0b00000010", start*20, s*(c%n));
+	text("x += 2;  // 0d2 = 0b00000010", start*20, s*(c%n));
 	for (let j=0; j<8; j++) {
 	    text(darray2[j], 6+(start+j)*20, h+16);
 	}
@@ -58,7 +66,7 @@ function draw() {
     }
     if (c%n==5) {
         fill('black')
-	text("→3        // 0d3 = 0b00000011", start*20, s*(c%n));
+	text("→3         // 0d3 = 0b00000011", start*20, s*(c%n));
 	for (let j=0; j<8; j++) {
 	    text(darray2[j], 6+(start+j)*20, h+16);
 	}
