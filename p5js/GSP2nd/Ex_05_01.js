c9c352d01c8a849b5ebf74d618c2ae47da614548
Hamada Tatsuyoshi <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-05-16 09:05:16 +0900

    Fix 05_01

--- p5js/GSP2nd/Ex_05_01.js
+++ p5js/GSP2nd/Ex_05_01.js
@@ -1,14 +1,5 @@
-let i=0;
-function setup(){
-    createCanvas(400, 300);
-    noLoop();
-    frameRate(10);
-}
 function draw() {
     background(0);
-    ellipse(20, 20, 20, 200-i);
-}
-
-function mouseClicked() {
-    i++;
+    print("I'm drawing");
+    print(frameCount);
 }
