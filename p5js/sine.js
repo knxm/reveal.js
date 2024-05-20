c14a6f310505b3f1cc6ee2b910077150002707de
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-05-21 08:42:05 +0900

    Add Ex_05_03

--- p5js/sine.js
+++ p5js/sine.js
@@ -1,16 +1,19 @@
 function setup() {
   createCanvas(400, 200); // Set the size of the window
-    strokeWeight(6);
 }
 
 function draw() {
-  background(0); // Set background color to white
-  translate(20, height/2); // Translate origin to the middle of the screen
+    background(0); // Set background color to white
+    strokeWeight(2);
+    stroke(255, 255, 0);
+    line(20, 0, 20, height);
+    line(0, height/2, width, height/2);
+    translate(20, height/2); // Translate origin to the middle of the screen
   
+ strokeWeight(6);
   stroke(255); // Set stroke color to black
   noFill(); // Don't fill the shape
 
-  
   beginShape();
   for (let x = 0; x < width; x += 5) {
     if (radians(x)<TWO_PI+0.01) {
