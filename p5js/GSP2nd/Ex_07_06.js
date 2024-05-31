b01e3bdabc3395ede816521337ec5b3301b7f9f0
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-05-31 11:41:49 +0900

    Fix some sizes

--- p5js/GSP2nd/Ex_07_06.js
+++ p5js/GSP2nd/Ex_07_06.js
@@ -11,7 +11,7 @@ function draw() {
   background(102);
   fill(255);
   textSize(24);
-  text("That's one small step for man...", 25, 60);
+  text("That's one small step for man...", 3*25/4.0, 3*60/4.0);
   textSize(12);
-  text("That's one small step for man...", 27, 90);
+  text("That's one small step for man...", 3*27/4.0, 3*90/4.0);
 }
