71b6cb6d0c85b67c7ed861990f55fa5856c2429b
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-06-06 15:27:53 +0900

    Change Ex_08_01.js

--- p5js/GSP2nd/Ex_08_01.js
+++ p5js/GSP2nd/Ex_08_01.js
@@ -1,4 +1,17 @@
-function draw() {
-  var fr = frameRate();  
-  print(fr);
+let i=0;
+let array = [0.0, 0.0, 0.0, 0.0, 0.0];
+function setup() {
+    createCanvas(240, 120);
+    textSize(24);
+    fill(240);
+    stroke(240);
+    //frameRate(1);
 }
+function draw() {
+    background(0);
+    array[0]=frameRate();
+    for (let i=0; i<4; i++) {
+	array[i+1]=array[i]
+	text(array[i], 10, 24*(i+1));
+    }
+ }
