2ee62f497882d3c3785b9abfc57aa2f50656aca1
Tatsuyoshi HAMADA <knxm@users.noreply.github.com>
2024-06-08 00:33:32 +0900

    Update ellipseMode.js

--- p5js/ellipseMode.js
+++ p5js/ellipseMode.js
@@ -12,7 +12,7 @@ function setup() {
     radio.option('RADIUS');
     radio.option('CORNER');
     radio.option('CORNERS');
-    radio.style("width", "102px");
+    radio.size(100);
     radio.selected('CENTER');
     radio.position(10, 10);
 }
