5fe166536bdfce2606b23b91ea94368ba2dc3f64
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-06-07 18:20:02 +0900

    Add change_frame

---
+++ p5js/change_fr.js
@@ -0,0 +1,17 @@
+let s, val;
+function setup() {
+    createCanvas(320, 80);
+    s = createSlider(0.5, 60);
+    s.position(10, 10);
+    s.size(120);
+    s.value(30);
+    textSize(24);
+}
+function draw() {
+    background(0);
+    stroke(240);
+    fill(240);
+    val=s.value();
+    frameRate(val);
+    text(frameRate(), 20, 60);
+}
