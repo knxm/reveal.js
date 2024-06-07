feb45fec7882bcd07c2b36c2823fd758c50f6b25
Tatsuyoshi HAMADA <knxm@users.noreply.github.com>
2024-06-08 00:54:12 +0900

    Update ellipseMode.js

--- p5js/ellipseMode.js
+++ p5js/ellipseMode.js
@@ -21,25 +21,25 @@ function draw() {
     val=radio.value();
     stroke(0);
     ellipse(cx, cy, rw*2, rh*2);
-    if (val==0) {
+    if (val=='0') {
 	stroke(0, 0, 200);
 	line(cx-rw, cy+rh+offset,  cx+rw, cy+rh+offset);
 	line(cx+rw+offset, cy-rh, cx+rw+offset, cy+rh);
 	ellipse(cx, cy, 6, 6);
     }
-    if (val==1) {
+    if (val=='1') {
 	stroke(0, 0, 200);
 	line(cx-rw, cy,  cx, cy);
 	line(cx, cy-rh, cx, cy);
 	ellipse(cx, cy, 6, 6);
     }
-    if (val==2) {
+    if (val=='2') {
 	stroke(0, 0, 200);
 	line(cx-rw, cy-rh,  cx-rw, cy+rh);
 	line(cx-rw, cy-rh, cx+rw, cy-rh);
 	ellipse(cx-rw, cy-rh, 6, 6);
     }
-    if (val==3) {
+    if (val=='3') {
 	stroke(150);
 	line(cx-rw, cy-rh,  cx-rw, cy+rh);
 	line(cx-rw, cy-rh, cx+rw, cy-rh);
