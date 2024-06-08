660760dae66e630313fadb3d513a97e05e989d03
Hamada Tatsuyoshi <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-06-09 01:06:28 +0900

    Change Ex_09_08

--- p5js/GSP2nd/Ex_09_08.js
+++ p5js/GSP2nd/Ex_09_08.js
@@ -1,10 +1,15 @@
 function setup() {
-  var yourWeight = 132;
-  var marsWeight = calculateMars(yourWeight);
-  print(marsWeight);
+    createCanvas(240, 60);
+    background(0);
+    fill(240);
+    stroke(240);
+    textSize(24);
+    var yourWeight = 132;
+    var marsWeight = calculateMars(yourWeight);
+    text(marsWeight.toPrecision(4), 10, 30);
 }
 
 function calculateMars(w) {
-  var newWeight = w * 0.38;
-  return newWeight;
+    var newWeight = w * 0.38;
+    return newWeight;
 }
