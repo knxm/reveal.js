17980af1638722631f028639fe9466afbf16bdf4
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-07-10 13:38:38 +0900

    Can't execute Ex_12_05.js

--- p5js/GSP2nd/Ex_12_05.js
+++ p5js/GSP2nd/Ex_12_05.js
@@ -1,5 +1,5 @@
-let films = [];
-let filmData;
+var films = [];
+var filmData;
 
 function preload() {
     filmData = loadJSON("media/films.json");
@@ -7,18 +7,18 @@ function preload() {
 
 function setup() {
     createCanvas(480, 120);
-    for (let i = 0; i < filmData.length; i++) {
-	let o = filmData[i];
-	films[i] = new Film(o);
+    for (var i = 0; i < filmData.length; i++) {
+      var o = filmData[i];
+      films[i] = new Film(o);
     }
     noStroke();
 }
 
 function draw() {
-    background(255);
-    for (let i = 0; i < films.length; i++) {
-	let x = i*32 + 32;
-	films[i].display(x, 105); 
+    background(0);
+    for (var i=0; i<films.length; i++) {
+	var x = i*32 + 32;
+	films[i].display(x, 105);
     }
 }
 
@@ -29,7 +29,7 @@ function Film(f) {
     this.rating = f.rating;
     
     this.display = function (x, y) {
-	let ratingGray = map(this.rating, 6.5, 8.1, 102, 255);
+	var ratingGray = map(this.rating, 6.5, 8.1, 102, 255);
 	push();
 	translate(x, y);
 	rotate(-QUARTER_PI);
