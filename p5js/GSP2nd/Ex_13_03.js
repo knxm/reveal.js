77f1aa47478ead6fdaf40616e3ad44272097403b
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-07-10 14:22:12 +0900

    Add Ex_13_03

--- p5js/GSP2nd/Ex_13_03.js
+++ p5js/GSP2nd/Ex_13_03.js
@@ -1,6 +1,19 @@
-function setup() {
+var sine;
+var freq = 400;
 
+function setup() {
+    createCanvas(440, 440);
+    sine = new p5.SinOsc();
+    sine.start();
 }
 function draw() {
-
+    background(0);
+    var hertz = map(mouseX, 0, width, 20.0, 440.0);
+    sine.freq(hertz);
+    stroke(204);
+    for (var x = 0; x < width; x++) {
+	var angle = map(x, 0, width, 0, TWO_PI*hertz);
+	var sinValue = sin(angle) * 120;
+	line(x, 0, x, height/2 + sinValue);
+    }
 }
