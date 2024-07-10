daca1026508983cf6d1dfaa0b5289a2bacc701be
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-07-10 14:01:24 +0900

    Add Ex_13_*.{html, js}

--- p5js/GSP2nd/Ex_13_01.js
+++ p5js/GSP2nd/Ex_13_01.js
@@ -1,52 +1,30 @@
-var pitch = 0.01
+var blip;
+
+var raidus = 120;
+var x = 0;
+var speed = 1.0;
+var direction = 1;
+
+function preload() {
+    blip = loadSound("media/blip.wav");
+}
 
 function setup(){
-  createCanvas(710, 400, WEBGL);
+    createCanvas(440, 440);
+    ellipseMode(RADIUS);
+    x = width/2;
 }
 
 function draw(){
-  background(250); 
-  translate(-250 * 2.5, 0, 0);
-  normalMaterial();
-  push();
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  plane(80);
-  pop();
-  translate(250, 0, 0);
-  push(); 
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  box(80, 80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  cylinder(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  cone(80, 80);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  torus(80, 20);
-  pop();
-  translate(250, 0, 0);
-  push();
-  rotateZ(frameCount * pitch);
-  rotateX(frameCount * pitch);
-  rotateY(frameCount * pitch);
-  sphere(80);
-  pop();
+    background(0);
+    x += speed * direction;
+    if ((x > width-radius) || (x < radius)) {
+	direction = -direction;
+	blip.play();
+    }
+    if (direction == 1) {
+	arc(x, 220, radius, radius, 0.52, 5.76);
+    } else {
+	arc(x, 220, radius, radius, 3.67, 8.9);
+    }
 }
---
+++ p5js/GSP2nd/Ex_13_02.js
@@ -0,0 +1,20 @@
+let mic;
+let amp;
+let scale = 1.0;
+
+function setup() {
+    createCanvas(440, 440);
+    background(0);
+    mic = new p5.AudioIn();
+    mic.start();
+    amp = new p5.Amptitude();
+    amp.setInput(mic);
+}
+function draw() {
+    noStroke();
+    fill(0, 10);
+    rect(0, 0, width, height);
+    scale=map(amp.getLevel(), 0, 1.0, 10, width);
+    fill(255);
+    ellipse(width/2, height/2, scale, scale);
+}
