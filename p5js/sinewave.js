af1be45057812d95aa82754eb5de54f2f018f863
Tatsuyoshi HAMADA <knxm@users.noreply.github.com>
2024-07-13 00:00:22 +0900

    Update sinewave.js

--- p5js/sinewave.js
+++ p5js/sinewave.js
@@ -2,25 +2,25 @@ let osc;
 let playing;
 
 function setup() {
-    let canvas = createCanvas(400, 120);
-    background(204);
-    canvas.mousePressed(playOscillator);
-    osc = new p5.Oscillator('sine');
-    osc.amp(0.5);
+  let canvas = createCanvas(400, 120);
+  background(204);
+  canvas.mousePressed(playOscillator);
+  osc = new p5.Oscillator('sine');
+  osc.amp(0.5);
 }
 function draw() {
-    background(0);
-    let hertz = map(mouseX, 0, width, 20.0, 440.0);
-    stroke(204);
-    for (var x = 0; x < width; x++) {
-	var angle = map(x, 0, width, 0, TWO_PI*hertz);
-	var sinValue = sin(angle) * 60;
-	line(x, 0, x, height/2 + sinValue);
-    }
-    if (playing) {
-      osc.freq(hertz);
-      osc.amp(1.0);
-    } 
+  background(0);
+  let hertz = map(mouseX, 0, width, 20.0, 440.0);
+  stroke(204);
+  for (var x = 0; x < width; x++) {
+  	var angle = map(x, 0, width, 0, TWO_PI*hertz);
+	  var sinValue = sin(angle) * 60;
+	  line(x, 0, x, height/2 + sinValue);
+  }
+  if (playing) {
+    osc.freq(hertz);
+		osc.amp(1.0);
+  } 
 }
 
 function playOscillator() {
@@ -28,6 +28,9 @@ function playOscillator() {
   playing = true;
 }
 
+function mousePressed() {
+  playOscillator();
+}
 
 function mouseReleased() {
   osc.amp(0, 0.5);
