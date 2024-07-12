9cdf6736969adfa5125ec12f2847bd6982804179
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-07-12 17:43:00 +0900

    Add sine

--- p5js/sine.js
+++ p5js/sine.js
@@ -1,25 +1,19 @@
+let osc;
 function setup() {
-  createCanvas(400, 200); // Set the size of the window
-}
+    let canvas = createCanvas(200, 100);
+    background(204);
 
+    osc = new p5.Oscillator('sine');
+    osc.amp(0.5);
+    osc.freq(440);
+    canvas.mousePressed(play);
+}
 function draw() {
-    background(0); // Set background color to white
-    strokeWeight(2);
-    stroke(255, 255, 0);
-    line(20, 0, 20, height);
-    line(0, height/2, width, height/2);
-    translate(20, height/2); // Translate origin to the middle of the screen
-  
- strokeWeight(6);
-  stroke(255); // Set stroke color to black
-  noFill(); // Don't fill the shape
 
-  beginShape();
-  for (let x = 0; x < width; x += 5) {
-    if (radians(x)<TWO_PI+0.01) {
-    let y = -sin(radians(x)) * 80; // Calculate y-coordinate using sin function
-    vertex(x, y);// Draw vertex
-    }
-  }
-  endShape();
 }
+
+function play() {
+    osc.start();
+    osc.stop(1);
+}
+    
