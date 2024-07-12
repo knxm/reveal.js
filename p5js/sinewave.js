0d3442969db0d5c5549e4e3811d6bf60d6fbb203
Tatsuyoshi HAMADA <hamada.tatsuyoshi@nihon-u.ac.jp>
2024-07-12 17:54:35 +0900

    Add sine wave

--- p5js/sinewave.js
+++ p5js/sinewave.js
@@ -1,16 +1,24 @@
 let osc;
 function setup() {
-    let canvas = createCanvas(200, 100);
+    let canvas = createCanvas(400, 120);
     background(204);
 
     osc = new p5.Oscillator('sine');
     osc.amp(0.5);
-    osc.freq(440);
     canvas.mousePressed(play);
 }
 function draw() {
-
+    background(0);
+    var hertz = map(mouseX, 0, width, 20.0, 440.0);
+    osc.freq(hertz);
+    stroke(204);
+    for (var x = 0; x < width; x++) {
+	var angle = map(x, 0, width, 0, TWO_PI*hertz);
+	var sinValue = sin(angle) * 60;
+	line(x, 0, x, height/2 + sinValue);
+    }
 }
+
 
 function play() {
     osc.start();
