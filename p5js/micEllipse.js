ff423a7f03ab430d172edd040a59c63c62e0f3db
Tatsuyoshi HAMADA <knxm@users.noreply.github.com>
2024-07-13 01:38:33 +0900

    Add micEllipse

---
+++ p5js/micEllipse.js
@@ -0,0 +1,19 @@
+let mic;
+let amp;
+let diameter=10;
+
+ function setup(){
+  let cnv = createCanvas(200, 400);
+  cnv.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  mic.start();
+}
+
+function draw(){
+  noStroke();
+  fill(26, 76, 102, 10);
+  rect(0, 0, width, height);
+  diameter=map(mic.getLevel(), 0, 0.1, 10, height*2)
+  fill(255);
+  ellipse(width/2, height/2, diameter, diameter);
+}
