c63c4ebd2a103ec88c9d3a78f7ed7b160455c8c0
HAMADA Tatsuyoshi <hamada.tatsuyoshi@nihon-u.ac.jp>
2026-01-28 09:57:28 +0900

    Fixed parabola_m

--- p5js/parabola_m.js
+++ p5js/parabola_m.js
@@ -1,23 +1,35 @@
+let n=500;
+function p(t) {
+  return t;
+}
+
+function q(t) {
+  return t*t/200-200;
+}
+
 function setup() {
-    createCanvas(800, 600);
-    strokeWeight(6);
+  createCanvas(800, 600);
+  //noLoop();
+  fill(204);
+  strokeWeight(4);
 }
+
 function draw() {
-    background(25, 25, 25);
-    let t=map(mouseX, 0, width, 0, 2*PI);
-    push();
-    translate(width/2, height/2);
-    noFill();
-    stroke(255);
-    arc(0, 0, 400, 400, -t, 0);
-    fill(0, 0, 255);
-    stroke(255, 255, 0);
-    ellipse(200*cos(-t), 200*sin(-t), 8, 8);
-    pop();
-}
-function x(t) {
-    t;
-}
-function y(t) {
-    t*t;
+  let m=map(mouseX, 0, width, -400, 400);
+  background(25, 25, 25);
+  stroke(255);
+  noFill();
+  push();
+  translate(width/2, height/2);
+  //line(-width/2, 0, width/2, 0);
+  //line(0, -height/2, 0, height/2);
+  beginShape();
+  for (let i=-400; i<m; i++) {
+    vertex(p(i), q(i));
+  }
+  endShape();
+  fill(0, 0, 255);
+  stroke(255, 255, 0);
+  ellipse(p(m), q(m), 8, 8);
+  pop();
 }
